9bbfa3275f81404749d5253fa1d70b8f64d71454
robertelving <vivalaklask@gmail.com>
2017-02-22 12:46:07 +0100

    test

---
+++ tests/test.js
@@ -0,0 +1,43 @@
+const Application = require('spectron').Application;
+const path = require('path');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+var electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
+
+if (process.platform === 'win32') {
+    electronPath += '.cmd';
+}
+
+var appPath = path.join(__dirname, '..');
+
+app = new Application({
+    path: electronPath,
+    args: [appPath]
+});
+
+global.before(function () {
+    chai.should();
+    chai.use(chaiAsPromised);
+});
+
+describe('Test Example', function () {
+    beforeEach(function () {
+        //console.log("test")
+        return app.start();
+    });
+
+    afterEach(function () {
+        return app.stop();
+    });
+
+    it('opens a window', function () {
+        return app.client.waitUntilWindowLoaded()
+            .getWindowCount().should.eventually.equal(2);
+    });
+
+    it('tests the title', function () {
+        return app.client.waitUntilWindowLoaded()
+            .getTitle().should.eventually.equal('');
+    });
+});
